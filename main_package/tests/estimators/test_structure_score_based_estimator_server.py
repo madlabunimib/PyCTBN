cf659c99f99f9bcf9c00422fad53776bd891c164
Luca Moretti <lucamoretti96@gmail.com>
2020-12-22 14:02:58 +0100

    FIxed typo

--- main_package/tests/estimators/test_structure_score_based_estimator_server.py
+++ main_package/tests/estimators/test_structure_score_based_estimator_server.py
@@ -29,7 +29,7 @@ class TestStructureScoreBasedEstimator(unittest.TestCase):
     def test_structure(self):
         #cls.read_files = glob.glob(os.path.join('../../data', "*.json"))
         self.importer = ji.JsonImporter("/home/alessandro/Documents/ctbn_cba/data/networks_and_trajectories_ternary_data_6.json", 'samples', 'dyn.str', 'variables', 'Time', 'Name')
-        self.s1 = sp.SamplePath(cls.importer)
+        self.s1 = sp.SamplePath(self.importer)
         self.s1.build_trajectories()
         self.s1.build_structure()
 
@@ -50,7 +50,7 @@ class TestStructureScoreBasedEstimator(unittest.TestCase):
 
 
         self.importer = ji.JsonImporter("/home/alessandro/Documents/ctbn_cba/data/networks_and_trajectories_ternary_data_6.json", 'samples', 'dyn.str', 'variables', 'Time', 'Name')
-        self.s1 = sp.SamplePath(cls.importer)
+        self.s1 = sp.SamplePath(self.importer)
         self.s1.build_trajectories()
         self.s1.build_structure()
 
