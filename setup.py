bf1c154658c3188e0b9aad3119376b95c68a4495
Filippo Martini <f.martini@campus.unimib.it>
2021-03-15 00:00:07 +0100

    Modify plot graph method

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='PyCTBN',
-      version='1.0',
+      version='2.1',
       url='https://github.com/philipMartini/PyCTBN',
       license='MIT',
       author=['Alessandro Bregoli', 'Filippo Martini','Luca Moretti'],
